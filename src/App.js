240be2cbd80f017ea07a1ce00d81cd7459f2c39d
Pablo Mariño Boga <pablo.marino.boga@gmail.com>
2023-07-04 23:08:08 +0200

    Solves 1st iteration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
+
+
+const users = [
+                  {
+                  lastName:'Doe',
+                  firstName:'John',
+                  gender: 'male',
+                  height: 1.82,
+                  birth: new Date("1990-09-04")},
+
+                  {
+                    lastName:'Doe',
+                    firstName:'Jane',
+                    gender: 'female',
+                    height: 1.85,
+                    birth: new Date("1978-02-23")},
+
+                  {
+                    lastName:'Doe',
+                    firstName:'Jane',
+                    gender: 'female',
+                    height: 1.75,
+                    birth: new Date("1985-12-04")},
+              ];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>lab-react-training </h1>
+      <h3>Iteration 1 | Component: IdCard</h3>
+      {users.map((item, index)=>{
+        return <IdCard key={index} lastName={item.firstName} firstName={item.lastName} gender={item.gender} height={item.height} birth={item.birth} picture={`https://randomuser.me/api/portraits/${item.gender==="male"?"men":"women"}/${Math.round(Math.random()*100)}.jpg`}></IdCard>
+      })}
+      <h3>Iteration 2 | Component: Greetings</h3>
     </div>
   );
 }
---
+++ src/components/IdCard.js
@@ -0,0 +1,17 @@
+import './IdCard.css';
+
+function IdCard({ picture, firstName, lastName, gender, height, birth }) {
+
+  return (
+    <div>
+      <img src={picture} alt={'profile picture of ' + firstName} />
+      <p><b>Firstname:</b> {firstName}</p>
+      <p><b>Lastname:</b> {lastName}</p>
+      <p><b>Gender:</b> {gender}</p>
+      <p><b>Height:</b> {`${height} m`}</p>
+      <p><b>Birth:</b> {birth.toDateString()}</p>
+    </div>
+  );
+}
+
+export default IdCard;
